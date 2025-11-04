39800979123cae0cc12e1aeb20a0ff8c6acb8783
J-S-M-Code <joacosuilar@gmail.com>
2025-11-03 23:08:48 -0300

    Estructura

---
+++ src/main/java/model/integradorsinteclados/IntegradorSinTecladosApplication.java
@@ -0,0 +1,13 @@
+package model.integradorsinteclados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradorSinTecladosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegradorSinTecladosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/model/integradorsinteclados/IntegradorSinTecladosApplicationTests.java
@@ -0,0 +1,13 @@
+package model.integradorsinteclados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorSinTecladosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
