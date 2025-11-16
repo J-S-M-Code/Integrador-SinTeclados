d1588af730544cd5e322781810d4a1bdcabbadd1
lukztmb <lucas.tamblay.bruh@gmail.com>
2025-11-16 19:09:27 -0300

    added GET endpoint for Tasks with Comments

--- src/main/java/infrastructure/controller/ProjectController.java
+++ src/main/java/infrastructure/controller/ProjectController.java
@@ -4,6 +4,7 @@ import application.dto.request.ProjectRequestDTO;
 import application.dto.request.TaskCommentRequestDTO;
 import application.dto.response.CommentResponseDTO;
 import application.dto.response.ProjectResponseDTO;
+import application.dto.response.TaskWithCommentsResponseDTO;
 import application.usecase.*;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.concurrent.ForkJoinPool;
 
 // A implementar
 
@@ -19,14 +21,12 @@ public class ProjectController {
 
     private final CreateProjectUseCase createProjectUseCase;
     private final AddCommentToTaskUseCase addCommentToTaskUseCase;
+    private final GetTaskByIdUseCase getTaskByIdUseCase;
 
-    public ProjectController(CreateProjectUseCase createProjectUseCase,
-                                    CreateTaskUseCase createTaskUseCase,
-                                    AddCommentToTaskUseCase addCommentToTaskUseCase,
-                                    GetTaskByIdUseCase getTaskByIdUseCase,
-                                    FindTaskUseCase findTaskUseCase) {
+    public ProjectController(CreateProjectUseCase createProjectUseCase, GetTaskByIdUseCase getTaskByIdUseCase, AddCommentToTaskUseCase addCommentToTaskUseCase) {
         this.createProjectUseCase = createProjectUseCase;
         this.addCommentToTaskUseCase = addCommentToTaskUseCase;
+        this.getTaskByIdUseCase = getTaskByIdUseCase;
     }
 
     // --- Endpoints de Proyectos ---
@@ -48,6 +48,8 @@ public class ProjectController {
         return ResponseEntity.created(location).body(response);
     }
 
+    // --- Endpoints de Comentarios ---
+
     @PostMapping("/projects/{projectId}/tasks/{taskId}/comments")
     public ResponseEntity<CommentResponseDTO> addCommentToTask(
             @PathVariable Long projectId, // No se usa, pero es parte de la URL
@@ -58,4 +60,15 @@ public class ProjectController {
 
         return ResponseEntity.status(201).body(response);
     }
+
+    @GetMapping("/projects/{projectId}/tasks/{taskId}")
+    public ResponseEntity<TaskWithCommentsResponseDTO> getTaskById(
+            @PathVariable Long projectId,
+            @PathVariable Long taskId,
+            @RequestParam(value = "comments", defaultValue = "false") boolean withComments) {
+
+        TaskWithCommentsResponseDTO response = getTaskByIdUseCase.execute(taskId, withComments);
+        return ResponseEntity.ok(response);
+    }
+
 }
